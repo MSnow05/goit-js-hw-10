49bd23cf112293638506e0283251685f86d73b40
msnow05 <140186070+MSnow05@users.noreply.github.com>
2024-02-19 20:57:35 +0100

    e

--- src/index.js
+++ src/index.js
@@ -1,59 +1,54 @@
-// Import funkcji fetch oraz bibliotek
-import { fetchCountries } from './fetchCountries';
-import debounce from 'lodash.debounce';
-import Notiflix from 'notiflix';
-
-const inputEl = document.getElementById('search-box');
-const listEl = document.querySelector('.country-list');
-const info = document.querySelector('.country-info');
-
-
-const handleSearchCountry = event => {
-    const searchCountry = event.target.value.trim();
-    listEl.innerHTML = '';
-  
-    if (searchCountry !== '') {
-      fetchCountries(searchCountry)
-        .then(data => {
-          if (2 <= data.length && data.length <= 10) {
-            const markup = data
-              .map(
-                country =>
-                  `<li class= "list-item"><img clas = "flag" src=${country.flags.png} width = 80px>  ${country.name.common} </li>`
-              )
-              .join('');
-  
-            listEl.insertAdjacentHTML('beforeend', markup);
-          }
-          if (data.length > 10) {
-            Notiflix.Notify.info(
-              'Too many matches found. Please enter a more specific name.'
-            );
-          }
-          if (data.length === 1) {
-            const countryInfo = data
-              .map(
-                country =>
-                  `<h2><img clas = "flag" src=${
-                    country.flags.png
-                  } width = 80px>  ${country.name.common} </h2>
-                  <p>Capital: ${country.capital}</p>
-                  <p>Population: ${country.population}</p>
-                  <p>Languages: ${Object.values(country.languages)}</p>`
-              )
-              .join('');
-  
-            listEl.insertAdjacentHTML('beforeend', countryInfo);
-          }
-        })
-        .catch(error => {
-          Notiflix.Notify.failure('Oops, there is no country with that name');
-        });
+const baseUrl = 'https://restcountries.com/v2/name';
+export function fetchCountries(name) {
+    return axios.get(`${baseUrl}/${name}?fields=name.official,capital,population,flags.svg,languages`).then((response) => response.data);
+}
+import notiflix from 'notiflix';
+export function renderNotification(message, priority) {
+    notiflix.Notify.failure(message);
+    if (message.includes('404')) {
+        notiflix.Notify.failure('Oops, there is no country with that name');
     }
-  };
-  const DEBOUNCE_DELAY = 300;
+}
+function renderCountries(countries) {
+  if (countries.length > 100) {
+      renderNotification('Too many matches found. Please enter a more specific name.', 'info');
+  } else if (countries.length >= 2) {
+      list.innerHTML = '';
+      renderCountries(countries);
+  } else if (countries.length === 1) {
+      renderCountryInfo(countries[0]);
+      list.innerHTML = '';
+  } else if (countries.length === 0) {
+      renderNotification('Oops, there is no country with that name', 'warning');
+  } else {
+      list.innerHTML = '';
+      renderCountries(countries);
+  }
+}
+function renderCountryInfo(country) {
+  const infoContainer = document.createElement('div');
+  infoContainer.className = 'country-info';
   
-  inputEl.addEventListener(
-    'input',
-    debounce(handleSearchCountry, DEBOUNCE_DELAY)
-  );
+  const flag = document.createElement('img');
+  flag.src = `${country.flags.svg}?width=30`;
+  flag.alt = `${country.name.official} flag`;
+  
+  const name = document.createElement('h2');
+  name.textContent = `Name: ${country.name.official}`;
+  
+  const capital = document.createElement('p');
+  capital.textContent = `Capital: ${country.capital}`;
+  
+  const population = document.createElement('p');
+  population.textContent = `Population: ${country.population.toLocaleString()}`;
+  
+  const languages = document.createElement('p');
+  languages.textContent = `Languages: ${country.languages.join(', ')}`;
+  
+  infoContainer.appendChild(flag);
+  infoContainer.appendChild(name);
+  infoContainer.appendChild(capital);
+  infoContainer.appendChild(population);
+  infoContainer.appendChild(languages);
+  document.body.appendChild(infoContainer);
+}
