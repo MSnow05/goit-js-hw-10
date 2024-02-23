ce7fbd564d20a4124336dcb20841b6bc0fd9acd2
msnow05 <140186070+MSnow05@users.noreply.github.com>
2024-02-23 20:49:45 +0100

    e

--- src/index.js
+++ src/index.js
@@ -1,23 +1,21 @@
-// Import funkcji fetch oraz bibliotek
+
 import { fetchCountries } from './fetchCountries';
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
 
-// Stałe
 const searchBox = document.querySelector('#search-box');
 const countryList = document.querySelector('#country-list');
 const countryInfo = document.querySelector('#country-info');
-const container = document.querySelector('.container'); // Container
+const container = document.querySelector('.container'); 
 
-// Funkcja czyszcząca zawartość container
 function clearContainer() {
   countryList.innerHTML = '';
   countryInfo.innerHTML = '';
 }
 
-// Funkcja do wyświetlania listy krajów
+
 function showCountryList(countries) {
-  clearContainer(); // Wyczyść istniejącą zawartość
+  clearContainer(); 
   if (countries.length > 10) {
     Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
     return;
@@ -38,52 +36,43 @@ function showCountryList(countries) {
   });
 }
 
-// Funkcja do wyświetlania informacji o kraju
+
 const showCountryInfo = function showCountryInfo(country) {
-  clearContainer(); // Wyczyść istniejącą zawartość
+  clearContainer(); 
   const card = document.createElement('div');
   card.classList.add('country-info-card');
 
-  // Dodanie flagi
+
   const flag = document.createElement('img');
   flag.src = country.flags.svg;
   flag.alt = `${country.name.common} flag`;
   flag.classList.add('country-flag');
   card.appendChild(flag);
 
-  // Dodanie nazwy kraju
   const name = document.createElement('span'); 
   name.classList.add(`${'country-name'}`);
   name.textContent = country.name.common;
   card.appendChild(name);
 
-  // Dodanie sekcji z detalami
   const details = document.createElement('ul');
   details.classList.add('country-details');
 
-  // Dodanie stolicy
   const capital = document.createElement('li');
   capital.innerHTML = `<span>Capital: </span>${country.capital}`;
   details.appendChild(capital);
 
-  // Dodanie populacji
   const population = document.createElement('li');
   population.innerHTML = `<span>Population: <span>${country.population}`;
   details.appendChild(population);
 
-  // Dodanie języków
   const languages = document.createElement('li');
   languages.innerHTML = `<span>Languages: </span>${Object.values(country.languages).join(', ')}`;
   details.appendChild(languages);
-
-  // Dodanie sekcji detali do karty
   card.appendChild(details);
 
-  // Dodanie karty do kontenera
   countryInfo.appendChild(card);
 }
 
-// Obsługa zdarzenia input
 searchBox.addEventListener('input', debounce(async () => {
   const name = searchBox.value.trim();
   if (!name) {
