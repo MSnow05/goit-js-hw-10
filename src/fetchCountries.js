bfa6ec564018355c96ba884e5b44220278ddbdb0
msnow05 <140186070+MSnow05@users.noreply.github.com>
2024-02-24 20:09:25 +0100

    e

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,5 +1,5 @@
 export default function fetchCountries(name) {
-  const url = `https://restcountries.com/v3.1/name/${name}?fields=name.official,capital,population,flags.svg,languages ⁠`;
+  const url = `https://restcountries.com/v3.1/name/{name}?fields=name.official,capital,population,flags.svg,languages ⁠`;
 
   return fetch(url)
       .then(response => response.json())
