16b674159bb55b2a9494105850c1d436bc56fae3
msnow05 <140186070+MSnow05@users.noreply.github.com>
2024-02-25 15:48:55 +0100

    e

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,10 +1,11 @@
-export default function fetchCountries(name) {
-  const url = 'https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages';
-
-  return fetch(url)
-      .then(response => response.json())
-      .catch(error => {
-          console.error('Error fetching data:', error);
-          throw error;
-      });
-}
+const BASE_URL = 'https://restcountries.com/v3.1';
+export const fetchCountries = countryName => {
+  return fetch(
+    `${BASE_URL}/name/${countryName}?fields=name,capital,population,flags,languages`
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error(respons.status);
+    }
+    return response.json();
+  });
+};
