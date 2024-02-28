0a587cda79878177fe98b634e65e0cf606c14383
msnow05 <140186070+MSnow05@users.noreply.github.com>
2024-02-28 13:06:38 +0100

    e

--- src/index.js
+++ src/index.js
@@ -10,26 +10,24 @@ searchBox.addEventListener('input', debounce(handleSearch, 300));
 
 function handleSearch() {
     const searchTerm = searchBox.value.trim();
-
     if (searchTerm === '') {
         clearResults();
         return;
     }
-
     fetchCountries(searchTerm)
         .then(data => {
             if (data.length > 10) {
                 Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
-            } if (data.length >= 2 && data.length <= 10) {
+            } else if (data.length >= 2 && data.length <= 10) {
                 return displayCountryList(data);
-            } if (data.length === 1) {
-               return displayCountryInfo(data[0]);
-            } if(data.status === 404){
-                Notiflix.Notify.failure('Oops, there is no country with that name');
-        return;
+            } else if (data.length === 1) {
+                return displayCountryInfo(data[0]);
             }
         })
-    }
+        .catch(error => {
+            Notiflix.Notify.failure(error.message);
+        });
+}
 function clearResults() {
     countryList.innerHTML = '';
     countryInfo.innerHTML = '';
